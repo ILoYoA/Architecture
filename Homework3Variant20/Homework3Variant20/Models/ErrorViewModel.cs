a6b0f8436a8d0579dac0ad486317a8e05f62c47a
Илья <mescheryakovio@student.bmstu.ru>
2026-06-04 00:33:36 +0300

    Третье дз

---
+++ Homework3Variant20/Homework3Variant20/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Homework3Variant20.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
